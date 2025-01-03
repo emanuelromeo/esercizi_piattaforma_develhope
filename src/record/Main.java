80f169aed1f10f7572c1f95ffda82a493eee0218
emanuelromeo <emanuel.romeo@hotmail.com>
2025-01-03 15:15:11 +0100

    Added record

---
+++ src/record/Main.java
@@ -0,0 +1,9 @@
+package record;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Punto a = new Punto(3, 2.5);
+
+    }
+}
---
+++ src/record/Punto.java
@@ -0,0 +1,4 @@
+package record;
+
+public record Punto(double x, double y) {
+}
